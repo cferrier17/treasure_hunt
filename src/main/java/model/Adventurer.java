3a27cbdb5122b66276fbeec590c12b75297bf05a
Ferrier <ferrier.corentin@wanadoo.fr>
2021-04-13 00:09:35 +0200

    Test single movements and its dependencies funcs

--- src/main/java/model/Adventurer.java
+++ src/main/java/model/Adventurer.java
@@ -2,11 +2,13 @@ package model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import model.ExplorationMap.Coordinates;
 import model.direction.Direction;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Adventurer {
     private String name;
     private Direction direction;
